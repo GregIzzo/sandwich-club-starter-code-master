20ef95088b5df261c4854a85bde1963da553cf2d
GregIzzo <greg.izzo@gmail.com>
2018-04-28 20:34:47 -0400

    feat: Parse Json, populate view
    Added code and layout to display info from JSON data, and designed
    layout to handle scrolling on small screen or landscape view

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +48,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +61,27 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        if (sandwich == null) return;
+        TextView aka = findViewById(R.id.also_known_tv);
+        TextView ingredients = findViewById(R.id.ingredients_tv);
+        TextView origin = findViewById(R.id.origin_tv);
+        TextView description = findViewById(R.id.description_tv);
+
+        aka.setText(myJoin(", ", sandwich.getAlsoKnownAs()) );
+        ingredients.setText( myJoin(", ", sandwich.getIngredients()));
+        origin.setText(sandwich.getPlaceOfOrigin());
+        description.setText(sandwich.getDescription());
 
     }
+    private String myJoin(String delim, List<String> list){
+        if (list == null || list.size() == 0) return "";
+        StringBuilder mySb = new StringBuilder();
+        for(String element:list){
+            mySb.append(element).append(delim);
+        }
+        //delim added after last element
+        return mySb.toString().substring(0,mySb.length()-(delim.length() ));
+    }
+
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,62 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Log.i(TAG, "GGGGGG::  parseSandwichJson: [" + json + "]");
+        //Sandwich(String mainName, List<String> alsoKnownAs, String placeOfOrigin, String description, String image, List<String> ingredients)
+        String mainName;
+        List<String> alsoKnownAs;
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients;
+
+        if (json == null) return null; //if data string is empty, return null
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+            mainName = (sandwichJson.getJSONObject("name").getString("mainName"));
+            alsoKnownAs = jsonArrayToStringArray(sandwichJson.getJSONObject("name").getJSONArray("alsoKnownAs"));
+            placeOfOrigin = sandwichJson.getString("placeOfOrigin");
+            description = sandwichJson.getString("description");
+            image =  sandwichJson.getString("image");
+            ingredients = jsonArrayToStringArray(sandwichJson.getJSONArray("ingredients"));
+            Log.i(TAG, "2222222@: parseSandwichJson: [" + json + "]");
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+
+    }
+    private static List<String> jsonArrayToStringArray(JSONArray jarray) {
+       List<String> returnArray = new ArrayList< >();
+        if (jarray == null) return returnArray;
+        int len = jarray.length();
+
+        for (int i=0; i<len; i++){
+            try {
+                returnArray.add( jarray.getString(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return returnArray;
+
     }
 }
